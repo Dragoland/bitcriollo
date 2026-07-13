7eeb13e7ad6a6d4d2132f80bdb5f9adbca9e29c2
Dragoland <norland.chavez333@gmail.com>
2026-07-13 15:55:22 -0400

    Refactor routing to use createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router';
-import { Layout } from './components/Layout';
-import Home from './pages/Home';
-import { ServiciosPage } from './pages/Servicios';
-import Perfil from './pages/Perfil';
-import Proceso from './pages/Proceso';
-import Stack from './pages/Stack';
-import Contacto from './pages/Contacto';
-import { Blog } from './pages/Blog';
-import { BlogPost } from './pages/BlogPost';
-import ScrollToTop from './components/ScrollToTop';
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import { Layout } from './components/Layout'
+import Home from './pages/Home'
+import Perfil from './pages/Perfil'
+import { ServiciosPage } from './pages/Servicios'
+import Proceso from './pages/Proceso'
+import Stack from './pages/Stack'
+import Contacto from './pages/Contacto'
+import { Blog } from './pages/Blog'
+import { BlogPost } from './pages/BlogPost'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'perfil', element: <Perfil /> },
+      { path: 'servicios', element: <ServiciosPage /> },
+      { path: 'proceso', element: <Proceso /> },
+      { path: 'stack', element: <Stack /> },
+      { path: 'contacto', element: <Contacto /> },
+      { path: 'blog', element: <Blog /> },
+      { path: 'blog/:slug', element: <BlogPost /> },
+    ]
+  }
+])
 
 function App() {
-  return (
-    <BrowserRouter>
-    <ScrollToTop />
-    <Routes>
-    <Route element={<Layout />}>
-    <Route path="/" element={<Home />} />
-    <Route path="/servicios" element={<ServiciosPage />} />
-    <Route path="/perfil" element={<Perfil />} />
-    <Route path="/proceso" element={<Proceso />} />
-    <Route path="/stack" element={<Stack />} />
-    <Route path="/contacto" element={<Contacto />} />
-    <Route path="/blog" element={<Blog />} />
-    <Route path="/blog/:slug" element={<BlogPost />} />
-    </Route>
-    </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
