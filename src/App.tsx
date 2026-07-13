3d7dbb4982ac100ecb18816ec1c988699094bea5
Dragoland <norland.chavez333@gmail.com>
2026-07-13 16:13:53 -0400

    Reorganize imports and restore Perfil route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Layout } from './components/Layout'
 import Home from './pages/Home'
-import Perfil from './pages/Perfil'
 import { ServiciosPage } from './pages/Servicios'
+import Perfil from './pages/Perfil'
 import Proceso from './pages/Proceso'
 import Stack from './pages/Stack'
 import Contacto from './pages/Contacto'
+import Software from './pages/Software'
 import { Blog } from './pages/Blog'
 import { BlogPost } from './pages/BlogPost'
 
@@ -15,11 +16,12 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'perfil', element: <Perfil /> },
       { path: 'servicios', element: <ServiciosPage /> },
+      { path: 'perfil', element: <Perfil /> },
       { path: 'proceso', element: <Proceso /> },
       { path: 'stack', element: <Stack /> },
       { path: 'contacto', element: <Contacto /> },
+      { path: 'software', element: <Software /> },
       { path: 'blog', element: <Blog /> },
       { path: 'blog/:slug', element: <BlogPost /> },
     ]
