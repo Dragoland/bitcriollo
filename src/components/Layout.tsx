c12b42704f70c32de908610c9e789ee0d74f6b54
Dragoland <norland.chavez333@gmail.com>
2026-07-13 15:57:10 -0400

    Add ScrollToTop component to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router'
 import { Navigation } from './Navigation'
 import { Footer } from './Footer'
+import ScrollToTop from './ScrollToTop'
 
 export const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background text-foreground font-mono antialiased">
-    <Navigation />
-    <main className="flex-1 pt-14">
-    <Outlet />
-    </main>
-    <Footer />
+      <ScrollToTop />
+      <Navigation />
+      <main className="flex-1 pt-14">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   )
 }
