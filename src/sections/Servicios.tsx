a41709dc4fca1e4b7ecf72ecc6db4e8becc86fb6
Dragoland <norland.chavez333@gmail.com>
2026-07-13 11:49:43 -0400

    Add files via upload

--- src/sections/Servicios.tsx
+++ src/sections/Servicios.tsx
@@ -16,7 +16,7 @@ import {
   FileText,
   Palette,
   Image,
-  FileUser,
+  User,
   Video,
   Gamepad2,
   Code,
@@ -183,7 +183,7 @@ const services: Service[] = [
     tag: "design",
   },
   {
-    icon: FileUser,
+    icon: User,
     name: "Creación de currículums digitales",
     description:
       "CV profesional en PDF, cartas de presentación, portafolios simples. Para que te veas bien sin gastar fortuna.",
