f3a8a59372c10753378a19eb39b985f3404b8f0a
Dragoland <norland.chavez333@gmail.com>
2026-07-13 15:55:53 -0400

    Refactor ScrollToTop to improve readability

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,7 +2,9 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router'
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation()
-    useEffect(() => window.scrollTo(0, 0), [pathname])
-    return null
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
 }
