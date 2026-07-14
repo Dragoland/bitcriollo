bf0646d38f1766956ededfe1f2386ae7f2910e50
Dragoland <norland.chavez333@gmail.com>
2026-07-13 20:55:13 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,6 @@ import Hero from '../sections/Hero';
 import Perfil from '../sections/Perfil';
 import Servicios from '../sections/Servicios';
 import SoftwareStore from '../sections/SoftwareStore';
-import Proceso from '../sections/Proceso';
-import Stack from '../sections/Stack';
 import LaVallita from '../sections/LaVallita';
 import Contacto from '../sections/Contacto';
 
@@ -14,8 +12,6 @@ export default function Home() {
     <Perfil />
     <Servicios />
     <SoftwareStore />
-    <Proceso />
-    <Stack />
     <LaVallita />
     <Contacto />
     </>
