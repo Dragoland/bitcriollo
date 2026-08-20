a07fbe98e86747e4ddf90cb03994d5ff6564e047
Dragoland <norland.chavez333@gmail.com>
2026-08-20 17:48:21 -0400

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router'
 import { Navigation } from './Navigation'
 import { Footer } from './Footer'
 import ScrollToTop from './ScrollToTop'
