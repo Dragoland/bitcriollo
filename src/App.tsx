e9a970bb5cff9dc4caa0d7c460c96b178004c1f3
Dragoland <norland.chavez333@gmail.com>
2026-08-20 17:48:52 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Layout } from './components/Layout'
 import Home from './pages/Home'
 import { ServiciosPage } from './pages/ServiciosPage'
